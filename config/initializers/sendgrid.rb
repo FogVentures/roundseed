e75f5f53433dd17c7c276c261c04280f443fa3bd
Mat Johnson <matjohnson@gmail.com>
2012-09-12 16:01:35 -0700

    send grid user

--- config/initializers/sendgrid.rb
+++ config/initializers/sendgrid.rb
@@ -4,7 +4,7 @@ begin
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => 'app6891846',
+    :user_name      => 'app6891846@heroku.com',
     :password       =>  Configuration[:sendgrid],
     :domain         => 'heroku.com'
     }
